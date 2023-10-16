c78061f7aa10e723a60dcf9de5ce1f4881e4db3d
Tommy May <tommymay37@gmail.com>
2023-10-16 12:22:32 -0400

    ci(ci/lint.yaml): added typescript type checking during linting process

--- src/mocks/serverUtilitiesMock.ts
+++ src/mocks/serverUtilitiesMock.ts
@@ -2,6 +2,6 @@
  * https://payloadcms.com/docs/admin/webpack#aliasing-server-only-modules
  */
 
-export function signStaticMapUrlHook () {}
-export function signStaticMapUrl () {}
-export function verifyGoogleReCaptcha () {}
+export function signStaticMapUrlHook (): void {}
+export function signStaticMapUrl (): void {}
+export function verifyGoogleReCaptcha (): void {}
